6be871116aed9f7cf6f228ff27013958bdb30cec
Gabriel Campos <gcampos@hyl.cl>
2023-07-19 18:00:46 -0400

    2do commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,77 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import Presupuesto from './components/Presupuesto.vue';
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <header>
+      <h1>Planificador de Gastos</h1>
+
+      <div class="contenedor-header contenedor sombra">
+        <Presupuesto
+
+        />
+      </div>
+      
+    </header>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  :root {
+    --azul: #3b82f6;
+    --blanco: #FFF;
+    --gris-claro: #F5F5F5;
+    --gris: #94a3b8;
+    --gris-oscuro: #64748b;
+    --negro: #000;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  *,
+  *:before
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--gris-claro);
+  }
+  h1 {
+    font-size: 4rem;
+  }
+  h2 {
+    font-size: 3rem;
+  }
+  header {
+    background-color: var(--azul);
+  }
+  header h1 {
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--blanco);
+    text-align: center;
+  }
+  .contenedor {
+    width: 90%;
+    max-width: 80rem;
+    margin: 0 auto;
+  }
+  .contenedor-header {
+    margin-top: -5rem;
+    transform: translateY(5rem);
+    padding: 5rem;
+  }
+  .sombra {
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    background-color: var(--blanco);
+    border-radius: 1.2rem;
+    padding: 5rem;
+  }
+
+
 </style>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,66 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const presupuesto = ref(0)
+
+</script>
+
+<template>
+    <form
+        class="presupuesto"
+    >
+        <div class="campo">
+            <label for="nuevo-presupuesto">Definir Presupuesto</label>
+            <input
+                id="nuevo-presupuesto"
+                class="nuevo-presupuesto"
+                placeholder="Añade tu presupuesto"
+                type="number"
+                v-model="presupuesto"
+            />
+        </div>
+
+        <input type="submit" value="Definir Presupuesto"/>
+    </form>
+</template>
+     
+
+<style scoped>
+    .presupuesto{
+        width: 100%;
+    }
+    .campo {
+        display: grid;
+        gap: 2rem;
+    }
+    .presupuesto label {
+        font-size: 2.2rem;
+        text-align: center;
+        color: var(--azul);
+    }
+    .presupuesto input[type="number"] {
+        background-color: var(--gris-claro);
+        border-radius: 1rem;
+        padding: 1rem;
+        border: none;
+        font-size: 2.2rem;
+        text-align: center;
+    }
+    .presupuesto input[type="submit"] {
+        background-color: var(--azul);
+        border: none;
+        padding: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 2rem;
+        color: var(--blanco);
+        font-weight: 900;
+        width: 100%;
+        transition: background-color 300ms ease;
+    }
+    .presupuesto input[type="submit"]:hover{
+        background-color: #1048A4;
+        cursor: pointer;
+    }
+
+</style>
